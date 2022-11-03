0d28f60adb3c499c2713f3ccf78fc06396960d64
Bilel Issaoui <bileli@dev-ops.tn>
2022-11-03 13:37:42 +0100

    add script rollout status - if rollout status failed, run rollback

--- k8s-rollout-status.sh
+++ k8s-rollout-status.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sleep 100s
+sleep 60s
 
-if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"*]];
+if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]];
 then
     echo "Deployment ${deploymentName} Rollout has Failed"
     kubectl -n default rollout undo deploy ${deploymentName}

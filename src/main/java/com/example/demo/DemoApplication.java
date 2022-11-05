0095acfaeaa05a6896897b38c405903285eb084d
Bilel Issaoui <bileli@dev-ops.tn>
2022-11-05 13:12:12 +0100

    init

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.devsecops;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NumericApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NumericApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
